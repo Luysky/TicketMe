b2c13d41fe8e62e6ea9695ddaaddf070eb3f3750
LuyskyHevs <luyet.thomas+hevs@gmail.com>
2020-10-14 21:14:16 +0200

    Debut de la structure de base

---
+++ app/src/main/java/hes/projet/ticketme/GestionUsers.java
@@ -0,0 +1,14 @@
+package hes.projet.ticketme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GestionUsers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestion_users);
+    }
+}
---
+++ app/src/main/java/hes/projet/ticketme/LectureTicket.java
@@ -0,0 +1,14 @@
+package hes.projet.ticketme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LectureTicket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lecture_ticket);
+    }
+}
