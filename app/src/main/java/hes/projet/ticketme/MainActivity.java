cacbacaafb6f3956aafb1950d29a7c1b921b5276
Benjamin Keller <bkeller@cmsgen.net>
2020-10-17 10:12:44 +0200

    Cleanup

--- app/src/main/java/hes/projet/ticketme/MainActivity.java
+++ app/src/main/java/hes/projet/ticketme/MainActivity.java
@@ -2,7 +2,10 @@ package hes.projet.ticketme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,13 @@ public class MainActivity extends AppCompatActivity {
 
     private void onCLick() {
 
+    }
+
+    public void clickEcranAccueil(View view) {
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
     }
 }
