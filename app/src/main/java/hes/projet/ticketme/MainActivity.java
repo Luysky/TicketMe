eee56fe46aae5ce147b8b63bd99ad9377a6719f8
Benjamin Keller <bkeller@cmsgen.net>
2020-11-13 17:09:22 +0100

    Cleanup and review MainActivity

--- app/src/main/java/hes/projet/ticketme/MainActivity.java
+++ app/src/main/java/hes/projet/ticketme/MainActivity.java
@@ -1,21 +1,31 @@
 package hes.projet.ticketme;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-import hes.projet.ticketme.ui.*;
+import hes.projet.ticketme.ui.LoginHomepageActivity;
+import hes.projet.ticketme.ui.TicketListActivity;
+import hes.projet.ticketme.ui.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        /*
+         * Check if user is logged in or not.
+         * If logged in redirect to ticket list else to login
+         */
+        Intent intent;
+        long userId = getLoggedInUserId();
 
+        if (userId == 0) {
+            intent = new Intent(this, LoginHomepageActivity.class);
+        } else {
+            intent = new Intent(this, TicketListActivity.class);
+        }
+
+        startActivity(intent);
     }
 }
